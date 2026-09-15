69976c8fb994e002138624ee299cc39c3fe1b795
Ahmad <ahmadharbi0x1337x@gmail.com>
2026-09-15 17:48:01 +0300

    Initial UI

---
+++ backend/schemas.js
@@ -0,0 +1,14 @@
+// const userSchema = {
+//   id: "string",
+//   userName: "string",
+//   family: "string",
+//   age: "number",
+// };
+
+// const taskSchema = {
+//   id: "string",
+//   userId: "string",
+//   title: "string",
+//   description: "string",
+//   status: "string | Enum",
+// };
